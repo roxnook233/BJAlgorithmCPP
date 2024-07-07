e1c8f01adc53d84e8f6dc08022183d67d0c54f2c
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-07-07 22:50:42 +0900

    2741.c

---
+++ 2741.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+    int inp;
+    scanf("%d", &inp);
+
+    for(int i=0; i<inp; i++) printf("%d\n", i+1);
+    return 0;
+}
