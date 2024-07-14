39c41e79b94c56d5a8833d007d4c2beadf311249
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-07-14 20:42:04 +0900

    30802.cpp

---
+++ 30802.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int N; 
+    int t_size[6];
+    int t, p;
+    cin >> N;
+    
+    for(int i = 0; i < sizeof(t_size)/sizeof(int); i++)
+        cin >> t_size[i];
+
+    cin >> t >> p;
+
+    int count = 0;
+    for(int i = 0; i < sizeof(t_size)/sizeof(int); i++){
+        if(t_size[i] != 0){
+            if(t_size[i]%t == 0) count += t_size[i]/t; //printf("%d", t_size[i]%t+1);
+            else count += t_size[i]/t + 1; //printf("%d", t_size[i]%t);
+        }
+    }
+
+    printf("%d\n", count);
+    printf("%d %d\n", N/p, N%p);
+
+    return 0;
+}
