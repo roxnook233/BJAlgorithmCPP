888517e7eb594a87af5467ab78afa1f7fe9f2a95
roxnook233 <87252115+roxnook233@users.noreply.github.com>
2024-08-05 23:13:05 +0900

    2742.cpp

---
+++ 2742.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <queue>
+using namespace std;
+
+int main(void){//BJ 2742
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int i = n; i > 0; i--){
+        cout << i <<'\n';
+    }
+    return 0;
+}
